1c68f78243cc3b179b7e05490de6d25c503703dc
Sebastian Lizarazo <sebastian.lizarazo@outlook.es>
2022-11-17 16:11:00 -0500

    Actualizacion archivos

---
+++ models/Producto.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const ProductoSchema = mongoose.Schema({
+  nombre: { type: String, required: true, trim: true },
+  precio: { type: String, required: true, trim: true },
+  estado: { type: Boolean, default: false },
+  creado: { type: Date, default: Date.now() },
+  categoria: { type: mongoose.Schema.Types.ObjectId, ref: "Categoria" },
+});
+
+module.exports = mongoose.model("Producto", ProductoSchema);
